4db6b294d5423e08f27c4da4e7f89d87439ce9e6
zerofancy <ntutn.top@gmail.com>
2025-02-09 17:37:28 +0800

    fix: app/src/main/java/top/ntutn/floatcompose/FloatingWindow.kt - 修复并引入Compose相关功能及生命周期管理

--- app/src/main/java/top/ntutn/floatcompose/FloatingWindow.kt
+++ app/src/main/java/top/ntutn/floatcompose/FloatingWindow.kt
@@ -1,8 +1,8 @@
 package top.ntutn.floatcompose
 
 import android.content.Context
-import android.graphics.Color
 import android.graphics.PixelFormat
+import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
 import android.view.Gravity
@@ -10,7 +10,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.FrameLayout
-import android.widget.Toast
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import top.ntutn.floatcompose.util.AppContextUtil
 import top.ntutn.floatcompose.util.dp
 
@@ -19,6 +29,29 @@ class FloatingWindow @JvmOverloads constructor(
     defStyleAttr: AttributeSet? = null,
     defStyleRes: Int = 0
 ) : FrameLayout(context, defStyleAttr, defStyleRes) {
+    class FloatingWindowLifecycleOwner : LifecycleOwner, SavedStateRegistryOwner {
+        private val registry = LifecycleRegistry(this)
+        private val savedStateRegistryController = SavedStateRegistryController.create(this)
+
+        override val lifecycle: Lifecycle = registry
+        override val savedStateRegistry: SavedStateRegistry
+            get() = savedStateRegistryController.savedStateRegistry
+
+        fun onShow() {
+            savedStateRegistryController.performRestore(null)
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_START)
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        }
+
+        fun onDismiss() {
+            savedStateRegistry.performSave(Bundle())
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+            registry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        }
+    }
+
     companion object {
         private const val TAG = "FloatingWindow"
     }
@@ -28,16 +61,26 @@ class FloatingWindow @JvmOverloads constructor(
     private var mView: View? = null
     private var windowManager: WindowManager? = null
 
+    private var viewTreeLifecycleOwner: FloatingWindowLifecycleOwner? = null
+
     private fun createView(context: Context): View {
-        val view = View(context).also {
+        val lifecycleOwner = FloatingWindowLifecycleOwner()
+        viewTreeLifecycleOwner = lifecycleOwner
+
+        val view = ComposeView(context).also {
             it.layoutParams = LayoutParams(300.dp, 200.dp)
-            it.setBackgroundColor(Color.RED)
+            it.setContent {
+                Button(onClick = {}) {
+                    Text("Hello World!")
+                }
+            }
         }
         return FrameLayout(context).also {
             it.addView(view)
-            it.setOnClickListener {
-                Toast.makeText(context, "test", Toast.LENGTH_SHORT).show()
-            }
+            // dirty hack, ComposeView need this
+            it.id = android.R.id.content
+            it.setViewTreeLifecycleOwner(lifecycleOwner)
+            it.setViewTreeSavedStateRegistryOwner(lifecycleOwner)
         }
     }
 
@@ -66,6 +109,7 @@ class FloatingWindow @JvmOverloads constructor(
         windowManager?.addView(view, layoutParams)
 
         view.setOnTouchListener(ItemViewTouchListener(layoutParam, windowManager))
+        viewTreeLifecycleOwner?.onShow()
         isShowing = true
     }
 
@@ -73,6 +117,7 @@ class FloatingWindow @JvmOverloads constructor(
         if (!isShowing) {
             return
         }
+        viewTreeLifecycleOwner?.onDismiss()
         try {
             windowManager?.removeView(mView)
             mView = null
